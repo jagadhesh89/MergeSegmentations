2931ae9bc52d581adcae13e6b42abdd3bfd43753
jagadhesh89 <jagadhesh89@gmail.com>
2025-02-06 10:35:24 -0600

    Add files via upload

--- cellmerge/cellmerge/cellmerge.py
+++ cellmerge/cellmerge/cellmerge.py
@@ -53,7 +53,7 @@ def check_type_for_merge(pan_type, mon_type):
     return matchflag
 
 
-def merge_coordinates(coords_method1, coords_method2, radius_threshold, pan_types, mon_types, pan_prob, mon_prob, updated_mon_types):
+def merge_coordinates(coords_method1, coords_method2, radius_threshold, pan_types, mon_types, pan_prob, mon_prob, updated_mon_types, mon_map, pan_map):
     merged_coords = []
     dim = len(coords_method1[0])
 
@@ -68,7 +68,11 @@ def merge_coordinates(coords_method1, coords_method2, radius_threshold, pan_type
     merged_indices_method2 = []
     merged_types = []
     merged_source = []
-
+    
+    equivocal_nuclei = []
+    merge_stats = {"Mon_merges":0, "Pan_merges":0, "Mon_Pan_merges":0}
+    
+    
     # For each coordinate in method1, find coordinates in method2 within the radius threshold
     for i, coord1 in enumerate(coords_method1):
         #print(len(coords_method1))
@@ -79,6 +83,11 @@ def merge_coordinates(coords_method1, coords_method2, radius_threshold, pan_type
             if distance <= radius_threshold:
                 pan_type = pan_types[i]
                 mon_type = mon_types[j]
+                merge_cells_str = mon_map[mon_type] + "-" + pan_map[pan_type]
+                if merge_cells_str not in merge_stats:
+                    merge_stats[merge_cells_str] = 1
+                else:
+                    merge_stats[merge_cells_str] += 1
                 merged_indices_method1.append(i)
                 merged_indices_method2.append(j)
                 coord2 = coords_method2[j]
@@ -88,16 +97,21 @@ def merge_coordinates(coords_method1, coords_method2, radius_threshold, pan_type
 
                 matchflag = check_type_for_merge(pan_type, updated_mon_types[j])
                 if matchflag == 0:
+                    if pan_prob[i] > 0.75 and mon_prob[j] > 0.75:
+                        equivocal_nuclei.append([str(coord1[0])+"-"+str(coord1[1]),str(coord2[0]) + "-" + str(coord2[1]),pan_map[pan_type],mon_map[mon_type], str(pan_prob[i]), str(mon_prob[j])])
                     if pan_prob[i] > mon_prob[j]:
                         merged_types.append(pan_type)
+                        merge_stats["Pan_merges"] += 1
                         merged_source.append(0)
                         continue
                     else:
                         merged_types.append(int(mon_type)+5)
+                        merge_stats["Mon_merges"] += 1
                         merged_source.append(1)
                         continue
                 else:
                     merged_types.append(int(mon_type)+5)
+                    merge_stats["Mon_Pan_merges"] += 1
                     merged_source.append(1)
 
 
@@ -105,32 +119,35 @@ def merge_coordinates(coords_method1, coords_method2, radius_threshold, pan_type
     # non-merged coordinates from both methods
     non_merged_coords1 = [coord for i, coord in enumerate(coords_method1) if i not in merged_indices_method1]
     non_merged_coords2 = [coord for i, coord in enumerate(coords_method2) if i not in merged_indices_method2]
+    
+    
     #non_merged_coords1 = []
     #non_merged_coords2 = []
 
 
-    return merged_coords, non_merged_coords1, non_merged_coords2, merged_indices_method1, merged_indices_method2, merged_types, merged_source
+    return merged_coords, non_merged_coords1, non_merged_coords2, merged_indices_method1, merged_indices_method2, merged_types, merged_source, equivocal_nuclei, merge_stats
 
-def main():
+def save_dict_as_csv(dictionary, filename):
+    df = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in dictionary.items()]))
+    df.to_csv(filename, index=False)
+
+
+if __name__ =="__main__":
     parser = argparse.ArgumentParser(description='merge_preds')
 
-    parser.add_argument("-p", "--model1pred",
-                        dest="model1",
-                        help="The path to the dat file of model1 (pannuke)",
+    parser.add_argument("-p", "--panfile",
+                        dest="pannuke",
+                        help="The path to the dat file of pannuke",
                         required=True)
-    parser.add_argument("-m", "--model2pred",
-                        dest="model2",
-                        help="The path to the dat file of model2 (monusac)",
-                        required=True)
-    parser.add_argument("-o", "--outputpath",
-                        dest="outdir",
-                        help="The path to output directory",
+    parser.add_argument("-m", "--monusac",
+                        dest="monusac",
+                        help="The path to the dat file of monusac",
                         required=True)
 
     inpArgs = parser.parse_args()
-    pan_preds = os.path.abspath(inpArgs.model1)
-    mon_preds = os.path.abspath(inpArgs.model2)
-    
+    pan_preds = os.path.abspath(inpArgs.pannuke)
+    mon_preds = os.path.abspath(inpArgs.monusac)
+
     slidename=inpArgs.pannuke.split("/")[-2].replace(".dat","")
 
     wsi_pred_mon = joblib.load(mon_preds)
@@ -148,8 +165,22 @@ def main():
 
     radius_threshold = 10
 
-    merged_coords, non_merged_coords1, non_merged_coords2, merged_indices_method1,merged_indices_method2, merged_types, merged_source = merge_coordinates(pan_centroids, mon_centroids, radius_threshold, pan_types, mon_types, pan_prob, mon_prob, updated_mon_types)
+    merged_coords, non_merged_coords1, non_merged_coords2, merged_indices_method1,merged_indices_method2, merged_types, merged_source, equivocal_nuclei, merge_stats = merge_coordinates(pan_centroids, mon_centroids, radius_threshold, pan_types, mon_types, pan_prob, mon_prob, updated_mon_types, mon_map, pan_map)
 
+    total_coords = len(merged_coords) + len(non_merged_coords1) + len(non_merged_coords2)
+    merge_stats["Unmerged_from_pan"] = len(non_merged_coords1)
+    merge_stats["Unmerged_from_mon"] = len(non_merged_coords2)
+    merge_stats["Merged_nuclei"] = len(merged_coords)
+    merge_stats["Total_Ensemble_Nuclei"] = total_coords
+    merge_stats["Equivocal_nuclei_perc"] = round((float(len(equivocal_nuclei)) / float(total_coords)) * 100, 2)
+    
+    save_dict_as_csv(merge_stats,  slidename + "_merge_report.csv")
+    
+    equivoval_merges = open(slidename + "_equivocal_nuclei.csv", "w")
+    for eacharray in equivocal_nuclei:
+        equivoval_merges.write(str(eacharray[0]) + "," + str(eacharray[1]) + "," + str(eacharray[2]) + "," + str(eacharray[3]) + "," + str(eacharray[4]) + "," + str(eacharray[5]) + "\n")
+    equivoval_merges.close()
+    
     combined_dict = {}
     combined_pan_ids = []
     combined_mon_ids = []
@@ -197,8 +228,5 @@ def main():
             type_req = int(wsi_pred_pan[eachpred]['type'])
             combined_dict[eachpred] = {'box':box_req, 'centroid':centroid_req,'contour':contour, 'prob':prob_req, 'type':type_req}
             
-    with open(outdir + "/" + slidename+ ".dat", 'wb') as file:
+    with open('/research/bsi/projects/urology/s209167.he_slides/images/segmentation/output/combined/' +slidename+ ".dat", 'wb') as file:
         pickle.dump(combined_dict, file)
-
-if __name__ =="__main__":
-    main()   
